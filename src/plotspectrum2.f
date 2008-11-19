d6ffba2045010ccdd8d758ab46ea9fa7517011e9
Nicolás Cardiel <nicocardiel@users.noreply.github.com>
2008-11-19 07:50:38 +0000

    Under GPLv3

--- src/plotspectrum2.f
+++ src/plotspectrum2.f
@@ -1,3 +1,21 @@
+C------------------------------------------------------------------------------
+C Copyright 2008 Nicolas Cardiel
+C
+C This file is part of kolores.
+C 
+C Kolores is free software: you can redistribute it and/or modify
+C it under the terms of the GNU General Public License as published by
+C the Free Software Foundation, either version 3 of the License, or
+C (at your option) any later version.
+C 
+C Kolores is distributed in the hope that it will be useful,
+C but WITHOUT ANY WARRANTY; without even the implied warranty of
+C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+C GNU General Public License for more details.
+C 
+C You should have received a copy of the GNU General Public License
+C along with kolores. If not, see <http://www.gnu.org/licenses/>.
+C------------------------------------------------------------------------------
         SUBROUTINE PLOTSPECTRUM2(NPSPEC,WL_SPEC,FLUX_SPEC,
      +   EFLUX_SPEC,FLUX_RESID,SPECNAME,LPERR,CERR)
         IMPLICIT NONE
